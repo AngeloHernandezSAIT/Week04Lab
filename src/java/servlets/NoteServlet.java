0ff6476cb159a82968beb508089e25fbf399bc77
Angelo Hernandez <122906206+AngeloHernandezSAIT@users.noreply.github.com>
2023-02-05 22:01:53 -0700

    Add files via upload

---
+++ src/java/servlets/NoteServlet.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import models.Note;
+
+/**
+ *
+ * @author Cyber
+ */
+public class NoteServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String edit = request.getParameter("edit");
+
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+            Note note = new Note(br.readLine(),br.readLine());
+            request.setAttribute("note", note);
+            br.close();
+        } catch (IOException e) {
+
+        }
+
+        if (edit == null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
+                    .forward(request, response);
+        } else {
+            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp")
+                    .forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Note note = new Note(request.getParameter("title"), request.getParameter("content"));
+
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        try {
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+            pw.println(note.getTitle());
+            pw.println(note.getContent());
+            pw.close();
+        } catch (IOException e) {
+
+        }
+
+        request.setAttribute("note", note);
+
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
+                .forward(request, response);
+    }
+}
---
+++ web/WEB-INF/viewnote.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : viewnote
+    Created on : 5-Feb-2023, 9:09:41 PM
+    Author     : Cyber
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Simple Note Keeper</title>
+    </head>
+    <body>
+        <h1>Simple Note Keeper</h1>
+        <h2>Edit Note</h2>
+        <form action="note" method="post">
+            <b>Title:</b> <input type="text" name="title" value="${note.title}"><br>
+            <b>Contents:</b><textarea name="content">${note.content}</textarea><br>
+            <input type="submit" value="Save">
+        </form>
+    </body>
+</html>
